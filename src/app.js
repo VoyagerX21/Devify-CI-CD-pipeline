982f5a62cb21d5d2dcb43d5097436dbaba7f1440
VoyagerX21 <khakse2gaurav2003@gmail.com>
2025-06-20 20:14:54 +0530

    Implemented the API documentation at api-docs/ path

--- src/app.js
+++ src/app.js
@@ -5,12 +5,16 @@ const webhookRoutes = require('./routes/webhookRoutes');
 
 // Initialize the Express application
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // Apply raw body parser middleware specifically for GitHub webhook endpoint
 // to handle raw JSON payloads
 app.use('/webhook/github', express.raw({ type: 'application/json' }));
 app.use('/webhook/gitlab', express.raw({ type: 'application/json' }));
 app.use('/webhook/bitbucket', express.raw({ type: 'application/json' }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Apply JSON body parser middleware for all other routes
 app.use(express.json());
