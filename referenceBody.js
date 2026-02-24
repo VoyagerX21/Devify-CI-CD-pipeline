cca296101a771cd93f8a168176351d20bd131210
VoyagerX21 <khakse2gaurav2003@gmail.com>
2026-02-25 03:39:45 +0530

    Added reference body for frontend dashboard and study purpose

---
+++ referenceBody.js
@@ -0,0 +1,226 @@
+const ln = {
+  ref: "refs/heads/main",
+  before: "566895ea4006838e1c0e09d3afc55dc6f59e85e9",
+  after: "60190e1c0e9c793163dd6b9075c4cbab392bcb6c",
+  repository: {
+    id: 1003710391,
+    node_id: "R_kgDOO9Nntw",
+    name: "PipelineHub",
+    full_name: "VoyagerX21/PipelineHub",
+    private: false,
+    owner: {
+      name: "VoyagerX21",
+      email: "khakse2gaurav2003@gmail.com",
+      login: "VoyagerX21",
+      id: 167705761,
+      node_id: "U_kgDOCf78oQ",
+      avatar_url: "https://avatars.githubusercontent.com/u/167705761?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/VoyagerX21",
+      html_url: "https://github.com/VoyagerX21",
+      followers_url: "https://api.github.com/users/VoyagerX21/followers",
+      following_url:
+        "https://api.github.com/users/VoyagerX21/following{/other_user}",
+      gists_url: "https://api.github.com/users/VoyagerX21/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/VoyagerX21/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/VoyagerX21/subscriptions",
+      organizations_url: "https://api.github.com/users/VoyagerX21/orgs",
+      repos_url: "https://api.github.com/users/VoyagerX21/repos",
+      events_url: "https://api.github.com/users/VoyagerX21/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/VoyagerX21/received_events",
+      type: "User",
+      user_view_type: "public",
+      site_admin: false,
+    },
+    html_url: "https://github.com/VoyagerX21/PipelineHub",
+    description:
+      "🔧 A secure Node.js backend that listens to GitHub, GitLab & Bitbucket events (🚀 push, 🔁 PR, 🔀 merge), triggers a mock CI/CD pipeline, verifies signatures, logs events, and exposes a status API — all backend, no frontend.",
+    fork: false,
+    url: "https://api.github.com/repos/VoyagerX21/PipelineHub",
+    forks_url: "https://api.github.com/repos/VoyagerX21/PipelineHub/forks",
+    keys_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/VoyagerX21/PipelineHub/teams",
+    hooks_url: "https://api.github.com/repos/VoyagerX21/PipelineHub/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/issues/events{/number}",
+    events_url: "https://api.github.com/repos/VoyagerX21/PipelineHub/events",
+    assignees_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/branches{/branch}",
+    tags_url: "https://api.github.com/repos/VoyagerX21/PipelineHub/tags",
+    blobs_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/git/refs{/sha}",
+    trees_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/statuses/{sha}",
+    languages_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/languages",
+    stargazers_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/subscription",
+    commits_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/compare/{base}...{head}",
+    merges_url: "https://api.github.com/repos/VoyagerX21/PipelineHub/merges",
+    archive_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/{archive_format}{/ref}",
+    downloads_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/downloads",
+    issues_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/issues{/number}",
+    pulls_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/notifications{?since,all,participating}",
+    labels_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/labels{/name}",
+    releases_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/VoyagerX21/PipelineHub/deployments",
+    created_at: 1750171769,
+    updated_at: "2026-02-24T21:40:17Z",
+    pushed_at: 1771969393,
+    git_url: "git://github.com/VoyagerX21/PipelineHub.git",
+    ssh_url: "git@github.com:VoyagerX21/PipelineHub.git",
+    clone_url: "https://github.com/VoyagerX21/PipelineHub.git",
+    svn_url: "https://github.com/VoyagerX21/PipelineHub",
+    homepage: "https://pipelinehub.khakse.dev/api-docs",
+    size: 13821,
+    stargazers_count: 0,
+    watchers_count: 0,
+    language: "JavaScript",
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: false,
+    has_pages: false,
+    has_discussions: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    allow_forking: true,
+    is_template: false,
+    web_commit_signoff_required: false,
+    has_pull_requests: true,
+    pull_request_creation_policy: "all",
+    topics: [
+      "api",
+      "backend",
+      "ci-cd",
+      "hmac-sha256",
+      "nodejs",
+      "stream",
+      "swagger",
+      "vcs",
+      "webhooks",
+    ],
+    visibility: "public",
+    forks: 0,
+    open_issues: 0,
+    watchers: 0,
+    default_branch: "main",
+    stargazers: 0,
+    master_branch: "main",
+  },
+  pusher: { name: "VoyagerX21", email: "khakse2gaurav2003@gmail.com" },
+  forced: false,
+  sender: {
+    login: "VoyagerX21",
+    id: 167705761,
+    node_id: "U_kgDOCf78oQ",
+    avatar_url: "https://avatars.githubusercontent.com/u/167705761?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/VoyagerX21",
+    html_url: "https://github.com/VoyagerX21",
+    followers_url: "https://api.github.com/users/VoyagerX21/followers",
+    following_url:
+      "https://api.github.com/users/VoyagerX21/following{/other_user}",
+    gists_url: "https://api.github.com/users/VoyagerX21/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/VoyagerX21/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/VoyagerX21/subscriptions",
+    organizations_url: "https://api.github.com/users/VoyagerX21/orgs",
+    repos_url: "https://api.github.com/users/VoyagerX21/repos",
+    events_url: "https://api.github.com/users/VoyagerX21/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/VoyagerX21/received_events",
+    type: "User",
+    user_view_type: "public",
+    site_admin: false,
+  },
+  created: false,
+  deleted: false,
+  base_ref: null,
+  compare:
+    "https://github.com/VoyagerX21/PipelineHub/compare/566895ea4006...60190e1c0e9c",
+  commits: [
+    {
+      id: "60190e1c0e9c793163dd6b9075c4cbab392bcb6c",
+      tree_id: "a40fcfaa229b41b04f98ae7d0181ed4e6ec2878f",
+      distinct: true,
+      message: "debugging2",
+      timestamp: "2026-02-25T03:13:10+05:30",
+      url: "https://github.com/VoyagerX21/PipelineHub/commit/60190e1c0e9c793163dd6b9075c4cbab392bcb6c",
+      author: [Object],
+      committer: [Object],
+      added: [],
+      removed: [],
+      modified: [Array],
+    },
+  ],
+  head_commit: {
+    id: "60190e1c0e9c793163dd6b9075c4cbab392bcb6c",
+    tree_id: "a40fcfaa229b41b04f98ae7d0181ed4e6ec2878f",
+    distinct: true,
+    message: "debugging2",
+    timestamp: "2026-02-25T03:13:10+05:30",
+    url: "https://github.com/VoyagerX21/PipelineHub/commit/60190e1c0e9c793163dd6b9075c4cbab392bcb6c",
+    author: {
+      name: "VoyagerX21",
+      email: "khakse2gaurav2003@gmail.com",
+      date: "2026-02-25T03:13:10+05:30",
+      username: "VoyagerX21",
+    },
+    committer: {
+      name: "VoyagerX21",
+      email: "khakse2gaurav2003@gmail.com",
+      date: "2026-02-25T03:13:10+05:30",
+      username: "VoyagerX21",
+    },
+    added: [],
+    removed: [],
+    modified: ["src/controllers/webhookController.js"],
+  },
+};
